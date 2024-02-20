17da76adfd8d6439f12ecb47dff16ed0bf2ac1c3
Hanif0610 <marbling1293@naver.com>
2024-02-21 00:24:33 +0900

    [ADD] Bean으로 등록 후 Autowired로 불러오기

--- chapter3/src/main/kotlin/com/microservices/chapter3/CustomerController.kt
+++ chapter3/src/main/kotlin/com/microservices/chapter3/CustomerController.kt
@@ -1,13 +1,19 @@
 package com.microservices.chapter3
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
+import java.util.concurrent.ConcurrentHashMap
 
 @RestController
 class CustomerController {
 
+    @Autowired
+    lateinit var customers: ConcurrentHashMap<Int, Customer>
+
     @RequestMapping(value = ["/customer"], method = arrayOf(RequestMethod.GET))
 //    fun getCustomer() = "hello from a controller" //결과 : hello from a controller
-    fun getCustomer() = Customer(1, "Kotlin") //결과 : {"id":1, "name":"Kotlin"}
+//    fun getCustomer() = Customer(1, "Kotlin") //결과 : {"id":1, "name":"Kotlin"}
+    fun getCustomer() = customers[2] //결과 : {"id": 2,"name": "Spring"}
 }
